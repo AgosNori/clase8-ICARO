ac2a985545fa6c7f5b15767cb3c46c9c058fd840
AgostinaN <agosnoriega2018@gmail.com>
2023-04-20 20:52:36 -0300

    Agregando todos los archivos

---
+++ app.js
@@ -0,0 +1,24 @@
+const { read } = require('./crud/read');
+const {create}=require('./crud/create');
+const data = process.argv;
+
+const comand = (comando, param1, param2) => {
+  
+    switch (comando) {
+        case "read":
+            console.log(read());
+            break;
+        case "create":
+            create(param1,param2);
+            //funcion para crear una tarea
+            break;
+        case "delete":
+            // funcion para eliminar una tarea
+            break;
+        default:
+            console.log("no se encontro ningun comando que conincida ");
+            break;
+    }
+
+}
+comand(data[2], data[3], data[4]);
